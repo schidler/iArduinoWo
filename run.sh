2aa3621b99ef75259c2a094635051500922401d5
BenChung <t.BenChung@gmail.com>
2013-06-26 22:09:40 +0800

    OK

--- run.sh
+++ run.sh
@@ -2,17 +2,17 @@
 
 REV=`svn up | grep "At revision" |  cut -d " " -f 3`
 
-REV1=`ps -ef | grep "iArduinoWo" | grep -v grep | awk '{print $2}'`
+REV1=`ps -ef | grep "iArduinoWo" | grep -v grep | awk '{print $2}' | head -1`
 
-if [ ! -z $REV ] && [ -n $REV1 ]; then
+if [ ! -z $REV ] && [ ! -z $REV1 ]; then
     echo "BYE!BYE! ^_^ "
     exit 0
 fi
 
-if [ -z $REV ] && [ -n $REV1 ]; then
-	forever stop /opt/node_js/iArduinoWo/app.js
+if [ -z $REV ] && [ ! -z $REV1 ]; then
+        forever stop /opt/node_js/iArduinoWo/app.js
 fi
 
-if [ -z $REV ]; then
-	forever start /opt/node_js/iArduinoWo/app.js
+if [ -z $REV ] || [ -z $REV1 ]; then
+        forever start /opt/node_js/iArduinoWo/app.js
 fi
