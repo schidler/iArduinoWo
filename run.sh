c6eafbc09c8a6eeea715d556b5d8161b63ba4987
BenChung <t.BenChung@gmail.com>
2013-06-26 15:36:40 +0800

    OK

--- run.sh
+++ run.sh
@@ -2,11 +2,17 @@
 
 REV=`svn up | grep "At revision" |  cut -d " " -f 3`
 
-if [ ! -z $REV ]; then
+REV1=`ps -ef | grep "iArduinoWo" | grep -v grep | awk '{print $2}'`
+
+if [ ! -z $REV ] && [ -n $REV1 ]; then
     echo "BYE!BYE! ^_^ "
     exit 0
 fi
 
-forever stop /opt/node_js/iArduinoWo/app.js
+if [ -z $REV ] && [ -n $REV1 ]; then
+	forever stop /opt/node_js/iArduinoWo/app.js
+fi
 
-forever start /opt/node_js/iArduinoWo/app.js
+if [ -z $REV ]; then
+	forever start /opt/node_js/iArduinoWo/app.js
+fi
